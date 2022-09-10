3fddcba19c5850f0b1bcfab05a48db68ce0ab1f7
John Evenden <johnthefightguy@gmail.com>
2022-09-10 19:49:41 -0400

    Created user and thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createNewThought,
+  updatethought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thoughts-controller");
+
+router.route("/").get(getAllThoughts).post(createNewThought);
+
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updatethought)
+  .delete(deleteThought);
+
+router
+  .route("/:id/thought/:thoughtId")
+  .post(addReaction)
+  .delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createNewUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/users-controller");
+
+router.route("/").get(getAllUsers).post(createNewUser);
+
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
