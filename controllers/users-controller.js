9ae78d3d1b68cd2ddf79c3f5617cfe588d2fda04
John Evenden <johnthefightguy@gmail.com>
2022-09-12 12:10:35 -0400

    Created getAllUsers in users-controller

---
+++ controllers/users-controller.js
@@ -0,0 +1,15 @@
+const { Users } = require("../models/user");
+
+module.exports = {
+  getAllUsers(req, res) {
+    Users.find({})
+      .populate({ path: "friends" })
+      .populate({ path: "thoughts" })
+      .select("-__v")
+      .then((dbUsersData) => res.json(dbUsersData))
+      .catch((err) => {
+        console.error({ message: err });
+        return res.status(500).json(err);
+      });
+  },
+};
